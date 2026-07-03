404c789208508c3d2add9f7863e9ae114f1c5aac
Hemilton Tuco <hemilton.tuco@gmail.com>
2026-07-03 17:19:07 -0300

    home

--- main.js
+++ main.js
@@ -148,48 +148,92 @@ const projects = [
 
 const publications = [
   {
-    title:'Em busca do registro da Terra Primitiva: proveniência de zircões detríticos em drenagens e rios atuais',
-    authors:'Equipe CGA et al.',
-    abstract:'Estudo voltado à proveniência de zircões detríticos em seções Meso-Paleoarqueanas do Cráton São Francisco, integrando geocronologia, proveniência sedimentar e interpretação da evolução crustal.',
-    link:'#',
-    style:'pub1'
+    "title": "Geochemistry, U-Pb and Sm-Nd isotopic constraints on Rhyacian metamafic-metaultramafic plutonic rocks in the southern São Francisco paleocontinent, Mineiro belt, Brazil: petrogenesis and tectonic implication",
+    "authors": "PINHEIRO, M.A.P.; ÁVILA, C.A.; MAGALHÃES, J.R.; TOLEDO, C.L.B.; SILVA, M.A.; BONGIOLO, E.M.; VILELA, F.T.; TEDESCHI, M.; LUVIZOTTO, G.L.; TEIXEIRA, W.",
+    "date": "08/12/2026",
+    "abstract": "Rochas máficas e ultramáficas são fundamentais para a compreensão da evolução da Terra, fornecendo informações sobre a formação crustal antiga, os processos orogênicos e a amalgamação de continentes. Este estudo examina a petrogênese e a história metamórfica do Cinturão Mineiro,...",
+    "link": "publicacoes/geochemistry-u-pb-and-sm-nd-isotopic-constraints-on-rhyacian-metamafic-metaultramafic-plutonic.html",
+    "image": "img/publicacoes/geochemistry-u-pb-and-sm-nd-isotopic-constraints-on-rhyacian-metamafic-metaultramafic-plutonic.jpg"
   },
   {
-    title:'Potencial do Território Brasileiro para Hidrogênio – Energia Limpa e Renovável',
-    authors:'Pesquisadores do CGA',
-    abstract:'Síntese técnica sobre potencial territorial, condicionantes geocientíficos e oportunidades para desenvolvimento de rotas energéticas limpas e renováveis no Brasil.',
-    link:'#',
-    style:'pub2'
+    "title": "New U-Pb zircon constraints from Nigeria on the Proterozoic evolution of the Wes Gondwana Orogen",
+    "authors": "CABY, R.; FERRÉ, E.C.; WIEDENBECK, M.; GANADE, C.E.",
+    "date": "27/05/2026",
+    "abstract": "O complexo de embasamento pré-cambriano nigeriano é um componente chave do Escudo Benino-Nigéria, localizado entre os crátons da África Ocidental e do Congo. Este estudo emprega geocronologia U-Pb em zircão para refinar...",
+    "link": "publicacoes/new-u-pb-zircon-constraints-from-nigeria-on-the-proterozoic-evolution-of-the-wes-gondwana-oroge.html",
+    "image": "img/publicacoes/new-u-pb-zircon-constraints-from-nigeria-on-the-proterozoic-evolution-of-the-wes-gondwana-oroge.jpg"
   },
   {
-    title:'Integração laboratorial e geodados para pesquisa aplicada em geociências',
-    authors:'Autores CGA / SGB',
-    abstract:'Discussão sobre integração entre infraestrutura laboratorial, bases de dados e fluxos de trabalho voltados à produção de conhecimento aplicado e suporte à tomada de decisão.',
-    link:'#',
-    style:'pub3'
+    "title": "Rapid drought-to-flood weather whiplash amplifies climate change governance failure",
+    "authors": "SÁNCHEZ-MURILLO, R.; BIRKEL, C.; GIMENO, L.; MEJÍA-GONZÁLEZ, M.A.; HERRERAA, D.A.; KIRCHHEIM, R.; GASTMANS, D.",
+    "date": "22/05/2026",
+    "abstract": "Os eventos climáticos extremos ocorridos em 2024-2025 na Europa e nas Américas expuseram como a interação de fatores atmosféricos pode desencadear inundações sem precedentes, com graves consequências socioeconômicas e am...",
+    "link": "publicacoes/rapid-drought-to-flood-weather-whiplash-amplifies-climate-change-governance-failure.html",
+    "image": "img/publicacoes/rapid-drought-to-flood-weather-whiplash-amplifies-climate-change-governance-failure.png"
   },
   {
-    title:'Soluções em P&amp;D para rochas e fluidos de bacias petrolíferas',
-    authors:'Equipe da Rede SGB',
-    abstract:'Apresenta capacidades de P&amp;D associadas às unidades da rede SGB para rochas e fluidos, com ênfase em ensaios, organização de dados e aplicação institucional.',
-    link:'#',
-    style:'pub4'
+    "title": "Lung cancer trend in the state of Rio Grande do Sul, Brazil: ecological study, 2000–2021",
+    "authors": "JUNG, J.C.; DA COSTA, J.S.D., DE OLIVEIRA, C.H.E.; DE SOUZA FILHO, O.A., PANIZ, V.M.V.",
+    "date": "19/05/2026",
+    "abstract": "Introdução: O câncer de pulmão é um dos tipos mais comuns de câncer em todo o mundo, representando 11,4% de todos os casos incidentes em 2020, além de constituir a principal causa de morte por câncer, com aproximadamente...",
+    "link": "publicacoes/lung-cancer-trend-in-the-state-of-rio-grande-do-sul-brazil-ecological-study-20002021.html",
+    "image": "img/publicacoes/lung-cancer-trend-in-the-state-of-rio-grande-do-sul-brazil-ecological-study-20002021.png"
   }
 ];
-
 const partnerLogos = [
-  { name:'SGB', label:'Serviço Geológico do Brasil' },
-  { name:'MME', label:'Ministério de Minas e Energia' },
-  { name:'Governo Federal', label:'Brasil' },
-  { name:'CGA', label:'Centro de Geociências Aplicadas' },
-  { name:'P&amp;D', label:'Rede de Pesquisa' },
-  { name:'Museu', label:'Ciências da Terra' }
+  { name: 'Petrobras', image: 'img/parceiros/petrobras.svg' },
+  { name: 'Vale', image: 'img/parceiros/vale.png' },
+  { name: 'Unicamp', image: 'img/parceiros/unicamp.png' },
+  { name: 'UnB', image: 'img/parceiros/unb.png' },
+  { name: 'ADIMB', image: 'img/parceiros/adimb.png' },
+  { name: 'ANSN/IRD', image: 'img/parceiros/ansn-ird.jpg' },
+  { name: 'IEPA', image: 'img/parceiros/iepa.png' },
+  { name: 'ANP', image: 'img/parceiros/anp.jpg' },
+  { name: 'EPE', image: 'img/parceiros/epe.png' },
+  { name: 'IAEA', image: 'img/parceiros/iaea.png' },
+  { name: 'Instituto Serra Pilheira', image: 'img/parceiros/instituto-serra-pilheira.png' },
+  { name: 'OCC', image: 'img/parceiros/occ.png' },
+  { name: 'Observatório Nacional', image: 'img/parceiros/observatorio-nacional.jpg' },
+  { name: 'Queen University Balfast', image: 'img/parceiros/queen-university-balfast.png' },
+  { name: 'Sanepar', image: 'img/parceiros/sanepar.svg' },
+  { name: 'UFAM', image: 'img/parceiros/ufam.png' },
+  { name: 'UFOP', image: 'img/parceiros/ufop.jpg' },
+  { name: 'UFPA', image: 'img/parceiros/ufpa.jpg' },
+  { name: 'UFPR', image: 'img/parceiros/ufpr.png' },
+  { name: 'UFSC', image: 'img/parceiros/ufsc.png' },
+  { name: 'UGA', image: 'img/parceiros/uga.png' },
+  { name: 'UNESP', image: 'img/parceiros/unesp.png' },
+  { name: 'Uniarp', image: 'img/parceiros/uniarp.png' },
+  { name: 'Unisinos', image: 'img/parceiros/unisinos.svg' },
+  { name: 'USP', image: 'img/parceiros/usp.jpg' },
+  { name: 'UTFPR', image: 'img/parceiros/utfpr.png' },
+  { name: 'Memorial University of Newfoundland', image: 'img/parceiros/memorial-university-of-newfoundland.png' },
+  { name: 'University of Alberta', image: 'img/parceiros/university-of-alberta.png' }
 ];
 
-const researchers = [
-  'Noevaldo','Carlos','Chris','Oderson','Rodrigo','Elias','Leandro','Maurício','Leonardo','Eugênio','Marco'
+const teamMembers = [
+  { name: 'Noevaldo de Araújo Teixeira', role: 'Pesquisador', position: 'Chefe do CGA', email: 'noevaldo.teixeira@sgb.gov.br', lattes: 'http://lattes.cnpq.br/3503261568861554', linkedin: 'https://www.linkedin.com/in/noevaldo-araujo-teixeira-8a355463/', researchgate: '' },
+  { name: 'Carlos Eduardo Ganade de Araújo', role: 'Pesquisador', position: 'Coordenador Executivo', email: 'carlos.ganade@sgb.gov.br', lattes: 'http://lattes.cnpq.br/5082464940811802', linkedin: 'https://www.linkedin.com/in/carlos-ca%C3%AA-ganade-45abb231/', researchgate: 'https://www.researchgate.net/profile/Carlos-Ganade' },
+  { name: 'Crisógono de Oliveira Vasconcelos', role: 'Pesquisador', position: 'Coordenador Executivo', email: 'crisogono.vasconcelos@sgb.gov.br', lattes: 'http://lattes.cnpq.br/9973751923987298', linkedin: 'https://www.linkedin.com/in/crisogono-vasconcelos-b92b472a/', researchgate: 'https://www.researchgate.net/profile/Crisogono-Vasconcelos' },
+  { name: 'Elias Martins Guerra Prado', role: 'Pesquisador', position: 'Coordenador Executivo', email: 'elias.prado@sgb.gov.br', lattes: 'http://lattes.cnpq.br/4551088996437373', linkedin: 'https://linkedin.com/in/eliasmgprado/?skipRedirect=true', researchgate: 'https://www.researchgate.net/profile/Elias-Prado-2' },
+  { name: 'Leandro Duarte Campos', role: 'Pesquisador', position: '', email: 'leandro.campos@sgb.gov.br', lattes: 'http://lattes.cnpq.br/1109157844409418', linkedin: 'https://www.linkedin.com/in/leandro-campos-17136a14/', researchgate: 'https://www.researchgate.net/profile/Leandro-Campos-3' },
+  { name: 'Oderson Antônio de Souza Filho', role: 'Pesquisador', position: '', email: 'oderson.souza@sgb.gov.br', lattes: 'http://lattes.cnpq.br/4433207125773280', linkedin: 'https://www.linkedin.com/in/oderson-souza-filho-b4a62746/', researchgate: 'https://www.researchgate.net/profile/Oderson-De-Souza-Filho' },
+  { name: 'Mauricio Liska Borba', role: 'Pesquisador', position: 'Coordenador Executivo', email: 'mauricio.borba@sgb.gov.br', lattes: 'http://lattes.cnpq.br/3697521511525277', linkedin: 'https://www.linkedin.com/in/maur%C3%ADcio-liska-borba-b407a679/', researchgate: 'https://www.researchgate.net/profile/Mauricio-Borba' },
+  { name: 'Leonardo Brenguere Leão Lopes', role: 'Pesquisador', position: '', email: 'leonardo.lopes@sgb.gov.br', lattes: 'http://lattes.cnpq.br/3473466080821646', linkedin: 'https://www.linkedin.com/in/leonardo-brenguere-le%C3%A3o-lopes-00b697b7/', researchgate: 'https://www.researchgate.net/profile/Leonardo-Lopes-2' },
+  { name: 'Beatriz Yuri Benetti Silva', role: 'Pesquisador', position: '', email: 'beatriz.benetti@sgb.gov.br', lattes: 'http://lattes.cnpq.br/5431485440557061', linkedin: 'https://www.linkedin.com/in/beatriz-benetti-b02733a1/', researchgate: 'https://www.researchgate.net/profile/Beatriz-Benetti' },
+  { name: 'Marco Aurelio Piacentini Pinheiro', role: 'Pesquisador', position: '', email: 'marco.pinheiro@sgb.gov.br', lattes: 'http://lattes.cnpq.br/0660128062017166', linkedin: '', researchgate: 'https://www.researchgate.net/profile/Marco-Aurelio-Pinheiro-2' },
+  { name: 'Roberto Kirchheim', role: 'Pesquisador', position: '', email: 'roberto.kirchheim@sgb.gov.br', lattes: 'http://lattes.cnpq.br/0997736239644674', linkedin: 'https://www.linkedin.com/in/roberto-kirchheim-5897b05/', researchgate: 'https://www.researchgate.net/profile/Roberto-Kirchheim' },
+  { name: 'Marcus Flavio Nogueira Chiarini', role: 'Pesquisador', position: '', email: 'marcus.chiarini@sgb.gov.br', lattes: 'http://lattes.cnpq.br/2953347496434887', linkedin: 'https://www.linkedin.com/in/marcus-flavio-nogueira-chiarini-9690001b9/', researchgate: 'https://www.researchgate.net/profile/Marcus_Chiarini' },
+  { name: 'Vinicius Morais Santana Matos', role: 'Pesquisador', position: '', email: 'vinicius.matos@sgb.gov.br', lattes: 'http://lattes.cnpq.br/9311270143564196', linkedin: 'https://www.linkedin.com/in/vin%C3%ADcius-matos-906a0465/', researchgate: '' },
+  { name: 'Rodrigo Rodrigues Adôrno', role: 'Pesquisador', position: '', email: 'rodrigo.adorno@sgb.gov.br', lattes: 'http://lattes.cnpq.br/0059421109255492', linkedin: 'https://www.linkedin.com/in/rodrigo-ad%C3%B4rno-253755a9/', researchgate: 'https://www.researchgate.net/profile/Rodrigo-Adorno' },
+  { name: 'Jean Carlo Henzel Taglieber', role: 'Pesquisador', position: '', email: 'jean.taglieber@sgb.gov.br', lattes: 'http://lattes.cnpq.br/3205802415368874', linkedin: 'https://www.linkedin.com/in/jean-carlo-henzel-taglieber-149924181/', researchgate: 'https://www.researchgate.net/profile/Jean-Henzel-Taglieber' },
+  { name: 'Robert de Lima Muniz', role: 'Pesquisador', position: '', email: 'robert.muniz@sgb.gov.br', lattes: 'http://lattes.cnpq.br/5739897977675955', linkedin: 'https://www.linkedin.com/in/robert-muniz-mining/', researchgate: '' },
+  { name: 'Kotaro Uchigasaki', role: 'Apoio Laboratorial e Administrativo', position: '', email: 'kotaro.uchigasaki@sgb.gov.br', lattes: 'http://lattes.cnpq.br/9255033424998609', linkedin: '', researchgate: '' },
+  { name: 'Lorrane Ramos de Oliveira da Costa', role: 'Apoio Laboratorial e Administrativo', position: '', email: 'lorrane.costa@sgb.gov.br', lattes: '', linkedin: '', researchgate: '' }
 ];
-const support = ['Kotaro','Mayse','Bruno','Lorrane','Rogério','Fernanda','Luiz'];
+
+const researchers = teamMembers.filter(member => member.role === 'Pesquisador');
+const support = teamMembers.filter(member => member.role !== 'Pesquisador');
 
 const projectTrack = document.getElementById('projectTrack');
 const publicationList = document.getElementById('publicationList');
@@ -200,6 +244,22 @@ const supportTeam = document.getElementById('supportTeam');
 
 function initials(name){
   return name.split(' ').map(n=>n[0]).slice(0,2).join('').toUpperCase();
+}
+
+function memberLinks(member){
+  const links = [
+    { label: 'Lattes', url: member.lattes },
+    { label: 'LinkedIn', url: member.linkedin },
+    { label: 'ResearchGate', url: member.researchgate }
+  ].filter(item => item.url);
+
+  if(!links.length) return '';
+
+  return `
+    <div class="member-links">
+      ${links.map(item => `<a href="${item.url}" target="_blank" rel="noopener noreferrer">${item.label}</a>`).join('')}
+    </div>
+  `;
 }
 
 function renderProjects(){
@@ -214,14 +274,15 @@ function renderProjects(){
 }
 
 function publicationCard(item, index){
-  // publicaçõess agora têm <a> com href para fallback/SEO
-  const href = `publication-detail.html?id=${index}`;
+  const image = item.image
+    ? `<img src="${item.image}" alt="Figura da publicação ${item.title.replaceAll('\"', '&quot;')}" loading="lazy">`
+    : `<span class="publication-thumb-fallback">CGA</span>`;
+
   return `
-    <a class="publication-card" data-type="publication" data-index="${index}" href="${href}">
-      <div class="publication-thumb ${item.style}"></div>
+    <a class="publication-card" data-type="publication" data-index="${index}" href="${item.link}">
+      <div class="publication-thumb">${image}</div>
       <div class="publication-text">
         <h3>${item.title}</h3>
-        <div class="publication-meta">${item.authors}</div>
         <p>${item.abstract}</p>
       </div>
     </a>
@@ -234,9 +295,10 @@ function renderPublications(){
 }
 
 function renderPartners(){
+  if(!partnerTrack) return;
   partnerTrack.innerHTML = partnerLogos.map(item => `
-    <div class="logo-slide" aria-label="${item.label}">
-      <div class="logo-mark">${item.name}<small>${item.label}</small></div>
+    <div class="logo-slide" aria-label="${item.name}">
+      <img src="${item.image}" alt="${item.name}" loading="lazy">
     </div>
   `).join('');
 }
@@ -246,11 +308,25 @@ function renderMembers(target, list){
     console.warn('renderMembers: target não encontrado');
     return;
   }
-  target.innerHTML = list.map(name => `
+  target.innerHTML = list.map(member => `
     <div class="member-card">
-      <div class="avatar">${initials(name)}</div>
-      <h4>${name}</h4>
-      <p>${target.id === 'supportTeam' ? 'Apoio técnico e administrativo' : 'Pesquisador(a) do CGA'}</p>
+      <div class="avatar">${initials(member.name)}</div>
+      <h4>${member.name}</h4>
+      <dl class="member-info">
+        <div>
+          <dt>Função</dt>
+          <dd>${member.role}</dd>
+        </div>
+        <div>
+          <dt>Cargo</dt>
+          <dd>${member.position || 'Não informado'}</dd>
+        </div>
+        <div>
+          <dt>E-mail</dt>
+          <dd><a href="mailto:${member.email}">${member.email}</a></dd>
+        </div>
+      </dl>
+      ${memberLinks(member)}
     </div>
   `).join('');
 }
@@ -417,13 +493,23 @@ function setupCarousel(rootSelector){
       return Math.max(0, track.children.length - visible);
     }
 
+    function visibleItems(){
+      const first = track.children[0];
+      if(!first) return 1;
+      return Math.max(1, Math.floor(wrap.clientWidth / first.getBoundingClientRect().width));
+    }
+
+    function pageStep(){
+      return Math.max(1, visibleItems() - 1);
+    }
+
     function update(){
       index = Math.max(0, Math.min(index, maxIndex()));
       track.style.transform = `translateX(${-index * stepWidth()}px)`;
     }
 
-    if(prev) prev.addEventListener('click', () => { index = Math.max(0, index - 1); update(); });
-    if(next) next.addEventListener('click', () => { index = Math.min(maxIndex(), index + 1); update(); });
+    if(prev) prev.addEventListener('click', () => { index = Math.max(0, index - pageStep()); update(); });
+    if(next) next.addEventListener('click', () => { index = Math.min(maxIndex(), index + pageStep()); update(); });
 
     window.addEventListener('resize', update);
     update();
